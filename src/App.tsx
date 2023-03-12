c55813b60f950c81ffd9e9efa16fde609269896c
Omar Baldi <omar.baldi@maersk.com>
2023-03-12 13:04:28 +0100

    Boxes rendered into the DOM with default value

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import './App.css';
 
+const DEFAULT_AMOUNT_ITEMS = 5;
+
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className='App'>
+      <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
+        {[...Array(DEFAULT_AMOUNT_ITEMS)].map((_, index: number) => {
+          return (
+            <div
+              key={`box-#${index}`}
+              style={{ height: '100px', width: '100px', border: '1px solid white' }}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
