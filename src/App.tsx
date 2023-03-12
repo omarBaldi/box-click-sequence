f9ea3af76b7e0cd043394f883ccba59597b44745
Omar Baldi <omar.baldi@maersk.com>
2023-03-12 13:09:11 +0100

    Implemented use of react useState hook to store the indexes of the boxes clicked respecting the order they have been clicked + conditional style applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useState } from 'react';
 import './App.css';
 
 const DEFAULT_AMOUNT_ITEMS = 5;
 
 function App() {
+  const [indexesBoxesClicked, setIndexesBoxesClicked] = useState<number[]>([]);
+
+  const handleBoxClick = ({ i: newIndex }: { i: number }): void => {
+    setIndexesBoxesClicked((prevIndexes) => [...prevIndexes, newIndex]);
+  };
+
   return (
     <div className='App'>
       <div style={{ display: 'grid', gridTemplateColumns: 'repeat(3, 1fr)' }}>
@@ -10,7 +17,18 @@ function App() {
           return (
             <div
               key={`box-#${index}`}
-              style={{ height: '100px', width: '100px', border: '1px solid white' }}
+              onClick={
+                indexesBoxesClicked.includes(index)
+                  ? undefined
+                  : () => handleBoxClick({ i: index })
+              }
+              style={{
+                height: '100px',
+                width: '100px',
+                border: '1px solid white',
+                cursor: 'pointer',
+                ...(indexesBoxesClicked.includes(index) && { backgroundColor: 'green' }),
+              }}
             />
           );
         })}
