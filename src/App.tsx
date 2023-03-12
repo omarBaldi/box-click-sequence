defc38836d708b4e90caa3b22ba09bc0016b5fdd
Omar Baldi <omar.baldi@maersk.com>
2023-03-12 13:22:48 +0100

    Implemented logic to revert boxes colors when all of them have been clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 const DEFAULT_AMOUNT_ITEMS = 5;
 
 function App() {
+  const [isRemoving, setIsRemoving] = useState<boolean>(false);
   const [indexesBoxesClicked, setIndexesBoxesClicked] = useState<number[]>([]);
+
+  useEffect(() => {
+    if (indexesBoxesClicked.length === DEFAULT_AMOUNT_ITEMS) {
+      setIsRemoving(true);
+    }
+
+    if (!isRemoving) return;
+
+    /**
+     * After having finished removing all of the indexes previously
+     * stored into the array (and in doing so reverted all the boxes
+     * to their default colors), reset the state value so I do not
+     * remove the index as soon as I click a new one.
+     */
+    if (indexesBoxesClicked.length <= 0) {
+      return setIsRemoving(false);
+    }
+
+    const removeOldestIndexFromBoxClicked = (): void => {
+      setIndexesBoxesClicked((prevIndexesClicked) => {
+        const [oldestIndexClicked, ...rest] = prevIndexesClicked;
+        return typeof oldestIndexClicked === 'undefined' ? prevIndexesClicked : [...rest];
+      });
+    };
+
+    const defaultTimeoutTime = 1000;
+    const timeout = setTimeout(removeOldestIndexFromBoxClicked, defaultTimeoutTime);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [indexesBoxesClicked, isRemoving]);
 
   const handleBoxClick = ({ i: newIndex }: { i: number }): void => {
     setIndexesBoxesClicked((prevIndexes) => [...prevIndexes, newIndex]);
@@ -18,7 +51,7 @@ function App() {
             <div
               key={`box-#${index}`}
               onClick={
-                indexesBoxesClicked.includes(index)
+                indexesBoxesClicked.includes(index) || isRemoving
                   ? undefined
                   : () => handleBoxClick({ i: index })
               }
